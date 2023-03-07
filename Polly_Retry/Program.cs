7daf548f4f84c0ffd525c836e8938f75293803cf
Andrew Santos Maia <andrew.maia@talktelecom.com.br>
2023-03-07 15:45:28 -0300

    primeiro commit

---
+++ Polly_Retry/Program.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+
+
+int retries = 1;
+var policy = Policy.Handle<Exception>().WaitAndRetryAsync(
+    3, // 3 tentativas
+    attempt => TimeSpan.FromMilliseconds(2000), // Espera 2 segundos entre cada tentativa
+    (exception, calculatedWaitDuration) => 
+    {
+        Console.WriteLine($"Erro tentativa {retries}: " + exception.Message);
+        retries++;
+    }
+);
+
+
+using (var client = new HttpClient())
+{
+    // O trecho abaixo será executado até 3x caso ocorram erros. A politica só irá "estourar" o erro depois da terceira tentativa
+    await policy.ExecuteAsync(async () =>
+    {
+            var msg = await client.GetStringAsync("www.g2.com.br/api/values/");
+    });
+}
